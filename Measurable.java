eba7dbaee5b2a167beb7c3209ad2d9f696ea3f79
Djibril Diop <90490243+jbtdiop@users.noreply.github.com>
2022-04-05 23:01:25 -0400

    Add files via upload

---
+++ Measurable.java
@@ -0,0 +1,13 @@
+package DDiop_lab_GenericLab;
+
+
+	public interface Measurable
+	{
+	   /**
+	      Computes the measure of the object.
+	      @return the measure
+	   */
+	   double getMeasure();
+
+	}
+
